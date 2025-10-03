fd833244ec5e545617cdde9460a3e117d888c47f
yayiji <hiyayiji@gmail.com>
2025-10-04 01:20:45 +0800

    add

--- src/lib/CommandPaletteSearch.svelte
+++ src/lib/CommandPaletteSearch.svelte
@@ -382,7 +382,7 @@
 
   .search-section {
     padding: 4px 8px;
-    border-bottom: 1px solid rgba(139, 125, 107, 0.4);
+    border-bottom: 1px solid rgba(139, 125, 107, 0.2);
   }
 
   .search-input-wrapper {
